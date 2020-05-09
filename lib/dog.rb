c490e0208cc8a3604abcd5d0b2d8fdf61da720ce
claudia v <claudia4ro@yahoo.com>
2020-05-09 15:58:28 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def name=(name)
+    @dog_name = name
+  end
+  
+  def name
+    @dog_name
+  end
+  
+  def breed= (breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+end
